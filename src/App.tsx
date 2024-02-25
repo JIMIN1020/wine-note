83fea9d74ae65186e72743204619d6284c23b34c
JIMIN1020 <jimins4920@gmail.com>
2024-02-25 22:16:49 +0900

    [Style]: global style 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import { GlobalStyle } from './styles/GlobalStyle';
 
 const App: React.FC = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-    </Routes>
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </>
   );
 };
 
---
+++ src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Login: React.FC = () => {
+  return <Container>Login</Container>;
+};
+
+export default Login;
+
+const Container = styled.div`
+  background-color: #45007b;
+`;
