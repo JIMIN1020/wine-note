73ebd985ae059f4e82ec8a81c60fb7b67b356008
JIMIN1020 <jimins4920@gmail.com>
2024-02-25 22:21:20 +0900

    [Setting]: font 세팅

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Login: React.FC = () => {
-  return <Container>Login</Container>;
+  return <Container>안녕하세요? hellloooo</Container>;
 };
 
 export default Login;
 
 const Container = styled.div`
   background-color: #45007b;
+  color: white;
 `;
